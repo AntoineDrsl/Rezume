68440263980d74cd7f5961bc93681bc5d8efb0be
Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2020-05-19 10:07:35 +0200

    FIX Remove from Favorites gestion

--- rezume-web/src/app/company/selected-cv-two/selected-cv-two.component.ts
+++ rezume-web/src/app/company/selected-cv-two/selected-cv-two.component.ts
@@ -13,6 +13,10 @@ export class SelectedCvTwoComponent implements OnInit {
 
   cvDetails;
 
+  companyDetails;
+
+  buttonAdd: boolean;
+
   constructor(private route: ActivatedRoute,
               private cvService: CvService,
               private _location: Location,
@@ -30,16 +34,52 @@ export class SelectedCvTwoComponent implements OnInit {
       err => {
         console.log('Impossible de recup cv details');
       }
-    )
+    );
+
+    this.checkIfFavorite();
   }
 
   backToPage() {
     this._location.back();
   }
 
+
+  checkIfFavorite() {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    this.companyService.getCompanyProfile().subscribe(
+      res => {
+        this.companyDetails = res['company'];
+        // On check si l'utilisateur possède deja ce cv en favori
+        if (this.companyDetails[0].favorites.length > 0) {
+
+          this.companyDetails[0].favorites.forEach(element => {
+            if (element === id) {
+              this.buttonAdd = false;
+            }
+          });
+        } else {
+          this.buttonAdd = true;
+        }
+
+      }
+    )
+  }
+
+
+
   addToFavorites() {
     this.companyService.addFavorite(this.cvDetails[0].student[0]._id).subscribe(
       res => {
+        this.buttonAdd = false;
+      }
+    );
+  }
+
+  removeFromFavorites() {
+    this.companyService.removeFavorite(this.cvDetails[0].student[0]._id).subscribe(
+      res => {
+        this.buttonAdd = true;
       }
     );
   }
